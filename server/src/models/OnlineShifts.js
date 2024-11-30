6e25a12ddb03dbf70e6f442ae1adfb4367054669
Ariel-Alegre <arielalegre98@gmail.com>
2024-11-30 14:13:03 -0300

    update

--- server/src/models/OnlineShifts.js
+++ server/src/models/OnlineShifts.js
@@ -14,65 +14,65 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
     },
     name: {
-        type: DataTypes.STRING,
-      },
-      lastName: {
-        type: DataTypes.STRING,
-      },
-      email: {
-        type: DataTypes.STRING,
-      },
-      phone: {
-        type: DataTypes.STRING,
-      },
-      age: {
-        type: DataTypes.STRING,
-      },
-      document_number: {
-        type: DataTypes.STRING,
-      },
-      reason_for_shift: {
-        type: DataTypes.STRING,
-      },
-      shifts: {
-        type: DataTypes.STRING,
-      },
+      type: DataTypes.STRING,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    phone: {
+      type: DataTypes.STRING,
+    },
+    age: {
+      type: DataTypes.STRING,
+    },
+    document_number: {
+      type: DataTypes.STRING,
+    },
+    reason_for_shift: {
+      type: DataTypes.STRING,
+    },
+    shifts: {
+      type: DataTypes.STRING,
+    },
 
-      date: {
-        type: DataTypes.STRING,
-      },
+    date: {
+      type: DataTypes.STRING,
+    },
 
-      time: {
-        type: DataTypes.STRING,
-      },
+    time: {
+      type: DataTypes.STRING,
+    },
 
-      specialty: {
-        type: DataTypes.STRING,
-      },
+    specialty: {
+      type: DataTypes.STRING,
+    },
 
-      status: {
-        type: DataTypes.STRING,
-      },
+    status: {
+      type: DataTypes.STRING,
+    },
 
-      userProfesionalId: {  // Clave foránea para la relación con UserProfessional
-        type: DataTypes.UUID, // Cambié el tipo de INTEGER a UUID
-        allowNull: false,
-        references: {
-          model: 'UserProfessionals', // La tabla relacionada (UserProfessionals)
-          key: 'id', // Clave primaria de UserProfessionals
-        },
+    userProfesionalId: {  // Clave foránea para la relación con UserProfessional
+      type: DataTypes.UUID, // Cambié el tipo de INTEGER a UUID
+      allowNull: true,  // Permite que el valor sea null
+      references: {
+        model: 'UserProfessionals', // La tabla relacionada (UserProfessionals)
+        key: 'id', // Clave primaria de UserProfessionals
       },
-    
- 
- 
+    },
 
-    
+
+
+
+
   }, {
     timestamps: false, // Agrega createdAt y updatedAt automáticamente
   }
-);
+  );
 
-  
+
 
   return OnlineShifts;
 };
