5a2db39879e71c14836287034904c6473829cdb7
Ariel-Alegre <arielalegre98@gmail.com>
2024-12-01 12:44:34 -0300

    update

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -3,13 +3,16 @@ const { Router  }= require('express');
 const router = Router();
 
 const {RegisterUser} = require("../controllers/RegisterUser")
-const {AllUsers} = require("../controllers/AllUsers")
+const {AllUsers} = require("../controllers/AllUsers");
+const {PutUsers} = require("../controllers/PutUsers");
+
 
 
 router.post('/register-user', RegisterUser);
 router.get('/all-user', AllUsers);
 
 
+router.put('/user/:userId', upload.single('image'), PutUsers);
 
 
 
