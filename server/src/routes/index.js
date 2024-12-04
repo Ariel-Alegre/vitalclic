f791cee518e7d710cea9ef1ec7bb9001b6e24d52
Ariel-Alegre <arielalegre98@gmail.com>
2024-12-04 20:21:35 -0300

    update

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,13 +4,14 @@ const router = Router();
 const routerProfesional = require("./prefessional");
 const routerUser = require("./user");
 const routerShift = require("./onlineShifts");
+const routerSede= require("./sede");
 
 
 
 
 
 
-router.use('/api', routerProfesional, routerUser, routerShift  ) 
+router.use('/api', routerProfesional, routerUser, routerShift, routerSede ) 
 
 
 
