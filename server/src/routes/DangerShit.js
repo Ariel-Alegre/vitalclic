878c3c43441945fb70386eaea65f64fea993daf7
Ariel-Alegre <arielalegre98@gmail.com>
2024-12-05 21:18:03 -0300

    update

--- server/src/routes/DangerShit.js
+++ server/src/routes/DangerShit.js
@@ -2,10 +2,9 @@
 const { Router  }= require('express');
 const router = Router();
 
-const {DangerShit} = require('../controllers/DangerShit');
 
 
-router.post('/danger-shifts', DangerShit);
+/* router.post('/danger-shifts', DangerShit); */
 
 
 
