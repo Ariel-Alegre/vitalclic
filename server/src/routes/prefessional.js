e7a8dd495fba1f3c25d4e6732754efb362c3705a
Ariel Alegre <arielalegre98@gmail.com>
2025-06-27 14:13:11 -0300

    update

--- server/src/routes/prefessional.js
+++ server/src/routes/prefessional.js
@@ -1,7 +1,6 @@
 
 const { Router  }= require('express');
 const router = Router();
-const https = require('https');
 
 
 
@@ -34,35 +33,7 @@ router.get('/datapersonal', DataPersonal );
 
 
 
-router.post('/token', async (req, res) => {
 
-    const { body, headers: { transactionid } } = req;
-    const options = {
-        host: 'sandbox-api-pw.izipay.pe',
-        port: 443,
-        path: '/security/v1/Token/Generate',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'transactionId': transactionid,
-        },
-        body: body,
-    };
-
-    const request = https.request(options, callback => {
-        callback.on('data', data => {
-            res.send(JSON.parse(data));
-        });
-    });
-
-    request.on('error', error => {
-        console.error(error);
-    });
-
-    if (req.body) request.write(JSON.stringify(req.body));
-    request.end();
-
-});
 
 
 
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,6 +1,7 @@
 
 const { Router  }= require('express');
 const router = Router();
+const https = require('https');
 
 const {RegisterUser} = require("../controllers/RegisterUser")
 const {AllUsers} = require("../controllers/AllUsers");
@@ -13,7 +14,35 @@ router.put('/user/:userId', upload.single('image'), PutUsers);
 router.post('/register-user', RegisterUser);
 router.get('/all-user', AllUsers);
 
+router.post('/token', async (req, res) => {
 
+    const { body, headers: { transactionid } } = req;
+    const options = {
+        host: 'sandbox-api-pw.izipay.pe',
+        port: 443,
+        path: '/security/v1/Token/Generate',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'transactionId': transactionid,
+        },
+        body: body,
+    };
+
+    const request = https.request(options, callback => {
+        callback.on('data', data => {
+            res.send(JSON.parse(data));
+        });
+    });
+
+    request.on('error', error => {
+        console.error(error);
+    });
+
+    if (req.body) request.write(JSON.stringify(req.body));
+    request.end();
+
+});
 
 
 
